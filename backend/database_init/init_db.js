68f7ccfd4e7e63ddd990d5ee541637fd54c65361
Baspel <brianovkolp@gmail.com>
2025-03-23 19:04:16 +0100

    fix: database connection

--- backend/database_init/init_db.js
+++ backend/database_init/init_db.js
@@ -1,44 +1,14 @@
 require('dotenv').config();
-const mysql = require('mysql2/promise');
 const fs = require('fs/promises');
 const path = require('path');
-const pool = null;
 
-async function initDB() {
+async function initDB(connection, pool) {
     console.log("Initializing database...");
 
     try {
-        const dbConfig = {
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            database: process.env.DB_NAME,
-            port: process.env.DB_PORT
-        };
-
-        if (process.env.DB_PASSWORD) {
-            dbConfig.password = process.env.DB_PASSWORD;
-        }
-
-        const connection = await mysql.createConnection(dbConfig);
-
-        connection.connect(function(err) {
-            if (err) {
-                console.error("Error connecting to database: ", err);
-                throw err;
-            }
-            console.log("Connected to database successfully!");
-        })
-
         const dbSQL = await fs.readFile('./database_init/create_database.sql', 'utf-8');
         await connection.query(dbSQL);
         console.log("Database created (if not exists)");
-
-        pool = mysql.createPool({
-            ...dbConfig, // Použití stejného nastavení
-            waitForConnections: true,
-            connectionLimit: 10,
-            queueLimit: 0
-        });
 
         async function runSQLFile(filePath) {
             const sql = await fs.readFile(filePath, 'utf-8');
@@ -64,4 +34,3 @@ async function initDB() {
 }
 
 module.exports = initDB;
-module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,45 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 const initDB = require('./database_init/init_db');
+const userRoutes = require('./routes/user');
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}
+
+const pool = mysql.createPool({
+    ...dbConfig,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
 async function startServer() {
     try {
-        await initDB();
+
+        if (process.env.DB_PASSWORD) {
+            dbConfig.password = process.env.DB_PASSWORD;
+        }
+
+        const connection = await mysql.createConnection(dbConfig);
+
+        if (!connection) {
+            throw new Error("Connection to database failed");
+        }
+        console.log("Connected to database successfully!");
+
+        await initDB(connection, pool);
         console.log("Database initialized successfully!");
+
+        app.use('/api/users', userRoutes);
 
         app.listen(3000, () => {
             console.log("Server is running on http://localhost:3000");
@@ -22,3 +52,5 @@ async function startServer() {
 }
 
 startServer();
+
+module.exports = pool;
